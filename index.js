028b4f83ccdf26f78c4d0e9971ac7aad42a0d01c
devilune <devilune@wizlog.io>
2019-04-28 20:29:47 +0900

    Change message

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const program = require('commander')
 
 program
-  .version('0.0.5', '-v, --version')
+  .version('0.0.6', '-v, --version')
   .parse(process.argv)
 
-console.log("redux-smith...")
+console.log("redux-smith")
